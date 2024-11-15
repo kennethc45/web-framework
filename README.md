f066cde15e47e60f2efd830dff196cb8d7db0b74
kennethc45 <kennethcadungog45@gmail.com>
2024-11-15 12:43:23 -0600

    Created Request file

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # web-framework
 Building Java web-framework from scratch
 
-# Testing Server File
- 1. Run `java -cp out tests.Server_Integration_Test`
+# Testing 
+For testing, if the test file is changed or intialized for the first time, make sure to compile it before running.
+
+For Server file
+ - Compile `javac -d out -cp "lib/*" src/Server.java tests/Server_Integration_Test.java`
+ - Run `java -cp out tests.Server_Integration_Test`
+
+For Request file
+ - Compile `javac -cp "lib/*" -d out src/Request.java tests/Request_Test.java`
+ - Run `java -ea -cp "out:lib/*" tests.Request_Test`
+ 
+
+If the test command does not work, then the compiled files might be outdated
+ - Run this to remove out folder `rm -rf out/`
+ - Recompile files you want to test and then run test commands
+
--- src/Request.java
+++ src/Request.java
@@ -8,7 +8,9 @@ import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.*;
 
+// Request class to group together HTTP request info
 public class Request {
     private String version;
     private String encoding;
@@ -22,18 +24,25 @@ public class Request {
     private String rawUrl;
 
     public Request(byte[] methodBytes, byte[] urlBytes, Map<String, String> headers, String version, byte[] bodyBytes, Object app) throws URISyntaxException {
+        // If no version is provided, then the parameter will be null
         this.version = (version != null) ? version : null;
         this.encoding = "utf-8";
+        // Decodes the byte arrays and then convert to string
         this.method = new String(methodBytes, StandardCharsets.UTF_8);
         this.rawUrl = new String(urlBytes, StandardCharsets.UTF_8);
         this.url = new URI(this.rawUrl);    // Java has no URL method for parsing URLs. URI is the closest
         this.headers = headers;
+        // If no body is provided, then the parameter will be null
         this.body = (bodyBytes != null) ? new String(bodyBytes, StandardCharsets.UTF_8) : null;
+        // If no app is provided, then the parameter will be null
         this.app = (app != null) ? app : null;
         this.matchInfo = new HashMap<>();
         this.queryParams = new HashMap<>();
+        // Populates the queryParams from the URl
+        parseQueryParams();
     }
 
+    // Getter Methods
     public Object getApp() {
         return app;
     }
@@ -42,16 +51,8 @@ public class Request {
         return queryParams;
     }
 
-    public void setQueryParams(Map<String, String> queryParams) {
-        this.queryParams = queryParams;
-    }
-
     public Map<String, String> getMatchInfo() {
         return matchInfo;
-    }
-
-    public void setMatchInfo(Map<String, String> matchInfo) {
-        this.matchInfo = matchInfo;
     }
 
     public String getMethod() {
@@ -70,14 +71,58 @@ public class Request {
         return body;
     }
 
+    // Setter Methods
+    public void setQueryParams(Map<String, String> queryParams) {
+        this.queryParams = queryParams;
+    }
+
+    public void setMatchInfo(Map<String, String> matchInfo) {
+        this.matchInfo = matchInfo;
+    }
+
+    // Parase query parameters from the URL
+    // https://example.com/path?name=Branch&products=[Universal%20Ads]
+    // Query params come after "?"
+    // Key value pairs are separated by "&"
+    // Ex: name=Branch  key - name; value - Branch
+    public void parseQueryParams() {
+        // Retrieve query component of URL
+        String query = url.getQuery();
+        // Check if URL's query can be found
+        if (query != null && !query.isEmpty()) {
+            // Split the query into key value pairs
+            String[] pairs = query.split("&");
+            // Iterage though each pair
+            for (String pair : pairs) {
+                // Split each pair into key and value
+                String[] keyValue = pair.split("=");
+                if (keyValue.length == 1) {
+                    queryParams.put(keyValue[0], "");
+                } else if (keyValue.length == 2) {
+                    // Insert a map entry for the query param key and value
+                    queryParams.put(keyValue[0], keyValue[1]);
+                }
+            }
+        }
+    }
+
     // Parses the body as JSON
     public Map<String, Object> getJson() throws JsonProcessingException {
         if (body != null) {
+            // Provides functionality for reading and writing JSON
             ObjectMapper objectMapper = new ObjectMapper(); 
-            // return objectMapper.readValue(body, Map.class);
+            // Parses JSON into Java object and then return it
+            // TypeReference<> specifies the type to deserialize the JSON to
             return objectMapper.readValue(body, new TypeReference<Map<String, Object>>() {});
         }
         return null;
     }
-    
+
+    // Returns unique ID/memory address for a request
+    @Override
+    public String toString() {
+        // System.identityHashCode() returns the hash code of the request
+        // Integer.toHexString() returns the string representation of the hash code 
+        return "<Request at " + Integer.toHexString(System.identityHashCode(this)) +">";
+    }
 }
